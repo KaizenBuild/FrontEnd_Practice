1eaba3c0a3defe71c8954e8cec9193a4e7915925
KaizenBuild <jyotirmay231@gmail.com>
2024-07-14 15:06:12 +0530

    Complete : Javascript Es6

---
+++ ReactJS Practice/2_ReactJS/src/components/ReactES6.js
@@ -0,0 +1,72 @@
+                // React ES6
+
+// What is ES6?
+// ES6 stands for ECMAScript 6.
+// it was published in 2015, and is also known as ECMAScript 2015.
+
+// --------------------------------------------------------------------------
+
+// Why Should I Learn ES6?
+// React uses ES6, and you should be familiar with some of the new features like:
+
+// Classes
+// Arrow Functions
+// Variables (let, const, var)
+// Array Methods like .map()
+// Destructuring
+// Modules
+// Ternary Operator
+// Spread Operator
+
+
+// --------------------------------------------------------------------------
+// React ES6 Classes
+
+// class Car{
+//     constructor(name){
+//         this.brand = name
+//     }
+// }
+// const myCar = new Car("BMW")
+// console.log(myCar.brand);
+
+// ----------------------------------------
+// Class Inheritance
+
+// class Car{
+//     constructor(name){
+//         this.brand = name;
+//     }
+
+//     present(){
+//         return 'I have a ' + this.brand;
+//     }
+// }
+
+// class Model extends Car{
+//     constructor(name, mod) {
+//         super(name);
+//         this.model = mod;
+//       }  
+//       show() {
+//           return this.present() + ', it is a ' + this.model
+//       }
+// }
+
+// const mycar = new Model("Ford", "Mustang");
+// console.log(mycar.show());
+
+// ---------------------------------------
+
+// The super() method refers to the parent class.
+
+// By calling the super() method in the constructor method, we call the parent's constructor method and get access to the parent's properties and methods.
+
+
+// --------------------------------------------------------------------------
+// --------------------------------------------------------------------------
+// --------------------------------------------------------------------------
+// --------------------------------------------------------------------------
+// --------------------------------------------------------------------------
+// --------------------------------------------------------------------------
+// --------------------------------------------------------------------------
