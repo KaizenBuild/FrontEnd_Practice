afb3eda9fdfeeba91f49eb4c979300cecae238eb
KaizenBuild <jyotirmay231@gmail.com>
2024-07-14 14:04:34 +0530

    Adding : React JS all topics name

---
+++ ReactJS Practice/2_ReactJS/src/components/ReactList.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+
+export const ReactList = () => {
+    return (
+        <ul>
+            <li><h2>What is react?</h2></li>
+
+            <br /> <hr />
+
+            <li><h2>React Component</h2></li>
+
+            <br /> <hr />
+
+            <li><h2>React Props</h2></li>
+
+            <br /> <hr />
+
+            <li><h2>React Events</h2></li>
+
+            <br /> <hr />
+
+            <li><h2>React Conditional Rendering</h2></li>
+
+            <br /> <hr />
+
+            <li><h2>React Lists</h2></li>
+
+            <br /> <hr />
+
+            <li><h2>React Forms</h2></li>
+
+            <br /> <hr />
+
+            <li><h2>React Router</h2></li>
+
+            <br /> <hr />
+
+            <li><h2>React Memo</h2></li>
+
+            <br /> <hr />
+
+            <li><h2>Styling React Using CSS</h2></li>
+
+            <br /> <hr />
+
+            <li><h2>React Hooks</h2>
+
+            <br /> <hr />
+
+                <ol>
+                    <li><h3>What is a Hook?</h3></li>
+
+                    <br /> <hr />
+                    
+                    <li><h3>React useState Hook</h3></li>
+
+                    <br /> <hr />
+                    
+                    <li><h3>React useEffect Hooks</h3></li>
+
+                    <br /> <hr />
+                    
+                    <li><h3>React useContext Hook</h3></li>
+
+                    <br /> <hr />
+                    
+                    <li><h3>React useRef Hook</h3></li>
+
+                    <br /> <hr />
+                    
+                    <li><h3>React useReducer Hook</h3></li>
+
+                    <br /> <hr />
+                    
+                    <li><h3>React useCallback Hook</h3></li>
+
+                    <br /> <hr />
+                    
+                    <li><h3>React useMemo Hook</h3></li>
+
+                    <br /> <hr />
+                    
+                    <li><h3>React Custom Hooks</h3></li>
+
+                    <br /> <hr />
+                    
+                </ol>
+            </li>
+        </ul>
+    )
+}
+
