5899759d19e4fb20b408cd6dc2ab679bbe58dc23
KaizenBuild <jyotirmay231@gmail.com>
2024-07-16 14:50:51 +0530

    Learning : React custom hooks

--- ReactJS Practice/2_ReactJS/src/components/ReactcustomHook.jsx
+++ ReactJS Practice/2_ReactJS/src/components/ReactcustomHook.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
 const ReactcustomHook = () => {
+  let def = [
+    "Hooks are reusable functions.",
+    "Custom Hooks start with 'use'. Example: useFetch.",
+    "A custom hook in React is a JavaScript function that starts with 'use' and can call other hooks. ",
+    "Custom hooks let you extract and reuse stateful logic between components.",
+    "we can build our own hook , according to our requirements",
+  ]
   return (
     <div>
-      
+      <ul>
+        {def.map((v, i) => <li key={i}>{v}</li>)}
+      </ul>
+
+
     </div>
   )
 }
